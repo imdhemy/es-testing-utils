d4e077777268117729648c468eab73d1140e1a97
Mohamad Eldhemy <imdhemy@gmail.com>
2022-08-19 15:26:37 +0300

    chore(tests): cover request exception with tests

--- src/RequestException.php
+++ src/RequestException.php
@@ -2,10 +2,22 @@
 
 namespace EsUtils;
 
+use GuzzleHttp\Exception\RequestException as GuzzleException;
 use GuzzleHttp\Psr7\Request;
 
-class RequestException extends \GuzzleHttp\Exception\RequestException
+/**
+ * Class RequestException
+ * This class is a wrapper for GuzzleHttp\Exception\RequestException.
+ */
+class RequestException extends GuzzleException
 {
+    /**
+     * Constructor.
+     * @param string $message The exception message
+     * @param string $method The HTTP method of the original request
+     * @param string $path The path of the original request
+     * @param array $headers The headers of the original request
+     */
     public function __construct(string $message, string $method = 'GET', string $path = '/_test', array $headers = [])
     {
         parent::__construct($message, new Request($method, $path, $headers));
---
+++ tests/Unit/RequestExceptionTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit;
+
+use EsUtils\RequestException;
+use Tests\TestCase;
+
+class RequestExceptionTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_create_an_instance_of_guzzle_request_exception()
+    {
+        $stub = new RequestException('test');
+        $this->assertInstanceOf(\GuzzleHttp\Exception\RequestException::class, $stub);
+    }
+}
