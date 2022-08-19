8aa4f04bd2aecb93809ce156846796cbe73b0e59
Mohamad Eldhemy <imdhemy@gmail.com>
2022-08-19 15:05:44 +0300

    chore(mocker): add docs

--- src/EsMocker.php
+++ src/EsMocker.php
@@ -8,13 +8,24 @@ use Elastic\Elasticsearch\Exception\AuthenticationException;
 use Psr\Http\Client\ClientExceptionInterface;
 use Psr\Http\Message\ResponseInterface;
 
+/**
+ * Class EsMocker
+ * This class is used to mock responses for Elasticsearch requests.
+ */
 class EsMocker
 {
     /**
      * @var ResponseInterface[]|ClientExceptionInterface[]
      */
-    private $mockResponses = [];
+    private array $mockResponses = [];
 
+    /**
+     * Creates a new EsMocker instance.
+     * @param array $body
+     * @param int $statusCode
+     *
+     * @return EsMocker
+     */
     public static function mock(array $body, int $statusCode = 200): EsMocker
     {
         $response = new Response(json_encode($body), $statusCode);
@@ -24,6 +35,13 @@ class EsMocker
         return $mocker;
     }
 
+    /**
+     * Enqueues a response to be returned when the next request is made.
+     * @param array $body
+     * @param int $statusCode
+     *
+     * @return EsMocker
+     */
     public function then(array $body, int $statusCode = 200): EsMocker
     {
         $response = new Response(json_encode($body), $statusCode);
@@ -32,6 +50,12 @@ class EsMocker
         return $this;
     }
 
+    /**
+     * Enqueues a request exception to be thrown when the next request is made.
+     * @param string $string
+     *
+     * @return $this
+     */
     public function fail(string $string): EsMocker
     {
         $requestException = new RequestException($string);
@@ -41,6 +65,7 @@ class EsMocker
     }
 
     /**
+     * Builds a client that uses the mocked responses.
      * @throws AuthenticationException
      */
     public function build(): Client
